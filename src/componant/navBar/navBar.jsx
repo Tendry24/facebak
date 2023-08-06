c78f8a64c6ebdc75c19fd8ffb4829951d2ed6985
tendryAxel <hei.tendry@gmail.com>
2023-08-06 16:24:45 +0300

    Test : MUI

--- src/componant/navBar/navBar.jsx
+++ src/componant/navBar/navBar.jsx
@@ -1,13 +1,18 @@
-import Logo from "../logo/logo"
-import ListeActionNav from "../listeActionNav/listeActionNav";
-
-import "./navBar.css"
+import { AppBar, Button, Toolbar, IconButton, Icon } from "@mui/material";
+import icon from "./logo192.png";
 
 const navBar = () => {
     return(
         <nav>
-            <Logo />
-            <ListeActionNav />
+            <AppBar position="static">
+                <Toolbar style={{display: "flex", flexDirection: "row",justifyContent: "space-between"}}>
+                    <Icon>
+                        <img src={icon} alt="Icon" style={{ height:'100%' }} />
+                    </Icon>
+                    <IconButton edge='start' color="inherit" aria-label="menu">Menu</IconButton>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
         </nav>
     )
 }
