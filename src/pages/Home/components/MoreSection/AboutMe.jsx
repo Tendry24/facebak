29be4f4cdddfd368a1aefbe3ef88baec877ff5da
tendryAxel <hei.tendry@gmail.com>
2023-08-11 18:58:38 +0300

    Fix : content of about me

--- src/pages/Home/components/MoreSection/AboutMe.jsx
+++ src/pages/Home/components/MoreSection/AboutMe.jsx
@@ -1,7 +1,13 @@
 import { Divider, Flex } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
+import React from "react";
 
 const AboutMe = () => {
+    const [pesronAttrubue, setPesronAttrubue] = React.useState([
+        {name: "Nom", value: "Rakoto"},
+        {name: "Prenom", value: "Axel"},
+    ]);
+
     return (
         <Flex
             flexDir={"column"}
@@ -13,6 +19,9 @@ const AboutMe = () => {
             gap={"8px"}>
             <Text>About me</Text>
             <Divider color="blue.500" size="lg" />
+            {pesronAttrubue.map(attribu=>{
+                return <Text>{attribu.name} : {attribu.value}</Text>
+            })}
         </Flex>
     )
 };
