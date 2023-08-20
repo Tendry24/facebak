5c652f2c57aa16325ffdfaf5214360967e8b4181
Kirisaki_VK <kirisaki.vk@gmail.com>
2023-08-20 20:07:12 +0300

    feat: added new routes and components for Login and SignUp

--- src/component/MainLayout.js
+++ src/component/MainLayout.js
@@ -2,11 +2,21 @@ import React from 'react'
 import {Flex} from "@chakra-ui/react";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Home/>
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+    {
+        path: "signup",
+        element: <SignUp/>
     }
 ])
 
---
+++ src/pages/SignUp/SignUp.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Flex} from "@chakra-ui/react";
+
+const SignUp = () => {
+    return (
+        /* Please do not touch this Flex,
+       it acts as an overlay to make the impression of being on another page
+       */
+        <Flex
+            w={"100vw"}
+            h={"100vh"}
+            position={"fixed"}
+            bg={"white"}
+            top={0}
+            left={0}
+            alignItems={"center"}
+            justifyContent={"center"}
+        >
+            SignUp
+        </Flex>
+    )
+}
+export default SignUp
