3ed8761ad0461a40e69885171394df6d3eb2d2a6
tendryAxel <hei.tendry@gmail.com>
2023-08-26 11:06:11 +0300

    fix : ignior case in search user on more section

--- src/services/utils.js
+++ src/services/utils.js
@@ -2,7 +2,7 @@ export const ilike = (string, other) => {
     if (string.length<other.length)
         return false;
     for (let i = 0;i<string.length-other.length;i++){
-        if (other == string.split("").slice(i,other.length+i).join(''))
+        if (other.toLowerCase() == string.split("").slice(i,other.length+i).join('').toLowerCase())
             return true;
     }
     return false;
