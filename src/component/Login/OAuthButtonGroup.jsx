56faf1215682db6d1fa54e5c6acc4a2d2fbe5a90
Tendry24 <hei.tendry.2@gmail.com>
2023-08-27 14:58:16 +0300

    fix : logo updated

--- src/component/Login/OAuthButtonGroup.jsx
+++ src/component/Login/OAuthButtonGroup.jsx
@@ -19,7 +19,7 @@ export const OAuthButtonGroup = () => (
   <ButtonGroup variant="secondary" spacing="4">
     {providers.map(({ name, icon }) => (
       <Button key={name} flexGrow={1}>
-        <VisuallyHidden>Sign in with {name}</VisuallyHidden>
+        <VisuallyHidden>Sign in with : {name}</VisuallyHidden>
         {icon}
       </Button>
     ))}
