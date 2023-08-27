d8e2947672f5749e8e6af1f5c2d5b1977bf0e261
Kirisaki_VK <kirisaki.vk@gmail.com>
2023-08-27 16:02:16 +0300

    fix: added fallback if user is not connected

--- src/App.js
+++ src/App.js
@@ -11,14 +11,19 @@ import {StorageProvider} from "./services/storage";
 
 function App() {
     const [self, setSelf] = useState({
-        username: "user-not_found",
-        photo: "user_profile"
+        username: "Not Connected",
+        photo: "/user.jpg"
     });
     useEffect(() => {
-        getUserById("7d54249c-552d-4f56-a298-51c97f2a51b8").then((res) => {
+        getUserById("7d54249c-552d-4f56-a298-51c97f2a51b9").then((res) => {
             setSelf(res.data)
             StorageProvider.setItem("self", res.data)
-        }).catch(e => console.log(e))
+        }).catch(
+            e => {
+                console.log(e);
+                StorageProvider.setItem("self", self)
+            }
+        )
     }, []);
     return (
         <Box>
